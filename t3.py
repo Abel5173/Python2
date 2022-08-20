87e0033dc1b9640f72a1539d9a0d4d9a997344c9
Abel Zeleke <abelzeleke5173@gmail.com>
2022-08-20 17:04:54 +0300

    python studying

---
+++ t3.py
@@ -0,0 +1,20 @@
+# Recursion
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
+    
+print(factorial(5))
+
+def is_even(x):
+    if x == 0:
+        return True
+    else:
+        return is_odd(x)
+
+def is_odd(x):
+    return not is_even(x)
+
+print(is_odd(17))
+print(is_even(23))
