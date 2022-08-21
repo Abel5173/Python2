e8668b797ac1ba0fde86a2dd8a988d865e24bd0b
Abel Zeleke <abelzeleke5173@gmail.com>
2022-08-21 18:47:30 +0300

    python soloearn

--- args.py
+++ args.py
@@ -3,11 +3,11 @@
 # Using *args as a function parameter enables you to pass an arbitrary number of arguments to that function. The arguments are then accessible as 
 # the tuple args in the body of the function.
 
-def function(named_arg, *args):
-    print(named_arg)
-    print(args)
+# def function(named_arg, *args):
+#     print(named_arg)
+#     print(args)
 
-function(1,2,3,4,5,6)
+# function(1,2,3,4,5,6)
 
 # **kwargs
 # **kwargs (standing for keyword arguments) allows 
@@ -17,7 +17,15 @@ function(1,2,3,4,5,6)
 # the keys are the argument names, and the values are 
 # the argument values.
 
-def my_func(x, y=7, *args, **kargs):
-    print(kargs)
+# def my_func(x, y=7, *args, **kargs):
+#     print(kargs)
 
-my_func(2,3,4,5,6,a=7,b=8)
+# my_func(2,3,4,5,6,a=7,b=8)
+
+def convert(num):
+    if num == 0:
+        return 0
+    return (num%2 + 10 * convert(num//2))
+
+a = int(input())
+print(convert(a))
