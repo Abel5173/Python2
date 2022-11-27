8cc2b482c4d66834c41dd3bca0459c577ff56aaf
Abel <abelzeleke5173@gmail.com>
2022-11-27 22:22:32 +0300

    leetcode good

--- leetcode/sort_colors.py
+++ leetcode/sort_colors.py
@@ -1,3 +1,9 @@
 l = [int(i) for i in input().split()]
-l.sort()
+x = []
+for i in range(len(l)):
+    min_idx = i
+    for j in range(i+1,len(l)):
+        if l[min_idx] > l[j]:
+            min_idx = j
+    l[i], l[min_idx] = l[min_idx], l[i]
 print(l)
