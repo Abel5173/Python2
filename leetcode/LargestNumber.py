603051305d7d69c5c965ddf8e5b487ed694d5d3e
Abel <abelzeleke5173@gmail.com>
2022-12-20 22:57:51 +0300

    Largest Numbers

--- leetcode/LargestNumber.py
+++ leetcode/LargestNumber.py
@@ -2,4 +2,9 @@ nums = [int(i) for i in input().split()]
 
 for i in range(len(nums)):
     for j in range(i+1, len(nums)):
-        
+        a = int(''.join([str(nums[i]), str(nums[j])]))
+        b = int(''.join([str(nums[j]), str(nums[i])]))
+        if b > a:
+            nums[i], nums[j] = nums[j], nums[i]
+s = ''.join(map(str, nums))
+print(s)
